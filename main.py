a65fd8fe2d0bd187534211e40017f6b60a2186c3
Kinga Mazur <kmzr9b67@gmail.com>
2025-02-24 15:10:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-import datetime
 from concurrent.futures.thread import ThreadPoolExecutor
+from datetime import datetime, timedelta
 
 from flask import Flask, render_template, request
 
-from Amondo import Amondo
-from CinemaScraper import CinemaScraper
-from Iluzjon import Iluzjon
+from amondo import Amondo
+from cinema_scraper import CinemaScraper
+from iluzjon import Iluzjon
 
+DAY_AFTER_TOMORROW = (datetime.now().date() + timedelta(2)).strftime('%a, %d.%m')
 DAYS = {
-    'TODAY': datetime.datetime.now().date(),
-    'TOMORROW': datetime.datetime.now().date() + datetime.timedelta(1),
-    'DAY AFTER TOMORROW': datetime.datetime.now().date() + datetime.timedelta(2)
+    'Today': datetime.now().date(),
+    'Tomorrow': datetime.now().date() + timedelta(1),
+    (datetime.now().date() + timedelta(2)).strftime('%a,'): 
+                                                    datetime.now().date() + timedelta(2)
 }
+
 
 app = Flask(__name__)
 
-
-def amondo(number):
-    CinemaScraper.result = []
+def get_amondo_data(number: str) -> None:
     cinema = Amondo()
-    cinema.retrive_movie_info(number)
-    return 0
+    cinema.retrive_movie_info(number) 
 
-
-def iluzjon(day_number):
+def get_iluzjon_data(day_number: int) -> None:
     cinema = Iluzjon()
     headings = cinema.html_parser().find_all('h3')
     try:
@@ -33,36 +32,35 @@ def iluzjon(day_number):
 
     show_table = cinema.find_elements_by_tag('table')[counter]
     show_table_hour = show_table.find_all(class_='hour')
-    time_and_title = [i.text.split(' - ') for i in show_table_hour]
+    time_and_title = [i.text.split(' - ', 1) for i in show_table_hour]
     list_times = [i[0] for i in time_and_title]
-    list_title = [i[1] for i in time_and_title]
-    years = [i.text.split(',') for i in show_table.find_all('i')]
-
-    if cinema.numer > cinema.id:
-        return 0
-
-    show_years = cinema.get_shows_list(years)
-    cinema.get_result_map(list_times, list_title, show_years)
-    return 0
+    list_titles = [i[1] for i in time_and_title]
+    list_years_html = [i.text.split(',') for i in show_table.find_all('i')]
+    list_years = cinema.get_shows_list(list_years_html)
+    cinema.get_result_map(list_times, list_titles, list_years)
 
 
-@app.route('/')
-def front():
-    return render_template('help.html')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html', day_after_tomorrow = DAY_AFTER_TOMORROW)
 
-@app.route('/final', methods=['GET', 'POST'])
-def final():
-    date = DAYS[request.args.get('day')]
+@app.route('/result', methods=['GET', 'POST'])
+def get_info():
+    day_get = request.args.get('day')
+    date = DAYS[day_get] 
+    CinemaScraper.result = []
 
     with ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(amondo, date)
-        executor.submit(iluzjon, int(date.day))
+        executor.submit(get_amondo_data, date)
+        executor.submit(get_iluzjon_data, int(date.day))
 
     repertuar = CinemaScraper.result
-    repertuar.sort(key=lambda x: str(x['rating']), reverse=True)
-
-    return render_template('index.html', post=repertuar)
-
+    repertuar.sort(key = lambda x: str(x['rating']), reverse = True)
+    
+    if day_get not in ['Today', 'Tomorrow']:
+        day_get = DAY_AFTER_TOMORROW
+    return render_template('day_schedule.html', post=repertuar, 
+                        what_day = day_get, what_date = date)
 
 if __name__ == '__main__':
     app.run()
