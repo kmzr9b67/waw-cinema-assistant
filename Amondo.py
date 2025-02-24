8a16743abb8668c880703a1fcc01c8ea296310bf
Kinga Mazur <kmzr9b67@gmail.com>
2025-02-24 15:07:50 +0100

    Update Amondo.py

--- Amondo.py
+++ Amondo.py
@@ -1,46 +1,45 @@
 from concurrent.futures.thread import ThreadPoolExecutor
 
-from CinemaScraper import CinemaScraper
-from Movie import Movie
-
+from movie import Movie
+from cinema_scraper import CinemaScraper
 
 class Amondo(CinemaScraper):
-    numer = 0
+    number = 0
     url = []
 
     def __init__(self):
         self.base_url = 'https://kinoamondo.pl/repertuar'
         super().__init__(self.base_url)
-        Amondo.numer += 1
-        self.cinema = 'amondo'
-        self.id = Amondo.numer
+        Amondo.number += 1
+        self.cinema = 'Amondo'
+        self.number = Amondo.number
 
 
-    def retrive_movie_info(self, number):
+    def retrive_movie_info(self, number: int) ->list:
         def __fetch_movie_info(url, time):
-            movie = Movie(base_url=url, time=time)
+            movie = Movie(base_url=url, time=time, cinema='Amondo')
             movie.set_title()
             movie.set_year()
-            movie.set_cinema('Amondo')
             movie.set_rating()
             return movie.to_dictionary()
 
         box = self.html_parser().find(id=f'schedule-{number}')
         try:
-            url_list = [i.find('a')['href'] for i in
+            urls_list = [i.find('a')['href'] for i in
                         box.find_all('div', class_='col-md-2 col-sm-3')]
         except AttributeError:
             return []
 
-        time_list = [i.text for i in box.find_all(class_='time')]
-        if Amondo.numer > self.id:
+        times_list = [i.text[-5:] for i in box.find_all(class_='time')]
+        # Handles multiple clicks when the user presses the button repeatedly
+
+        if Amondo.number > self.number:
             return 0
-
-        with ThreadPoolExecutor(len(url_list)) as executor:
+        
+        with ThreadPoolExecutor(len(urls_list)) as executor:
             for mapa in executor.map(__fetch_movie_info,
-                                     url_list, time_list):
+                                     urls_list, times_list):
                 CinemaScraper.result.append(mapa)
-
         return 0
 
 
