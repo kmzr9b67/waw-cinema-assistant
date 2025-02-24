3cc4271a1e7b594445054ae1828a2a79d6e17339
Kinga Mazur <kmzr9b67@gmail.com>
2025-02-24 15:08:22 +0100

    Update and rename CinemaScraper.py to cinema_scraper.py

--- cinema_scraper.py
+++ cinema_scraper.py
@@ -9,18 +9,21 @@ class CinemaScraper:
         self.base_url = base_url
         self.session = requests.Session()
         self._html = None
+        
 
-    def make_request(self):
+    def make_request(self) -> str:
         return requests.get(self.base_url).text
 
-    def html_parser(self):
+    def html_parser(self) -> str:
         if self._html is None:
             self._html = BeautifulSoup(self.make_request(),
                                        'html.parser')
+            
         return self._html
 
-    def find_elements_by_tag(self, tag):
+    def find_elements_by_tag(self, tag: str) -> str:
         if self._html is None:
             self._html = BeautifulSoup(self.make_request(),
                                        'html.parser')
+            
         return self._html.find_all(tag)
